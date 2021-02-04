8f5b4fbc2318568968d370e734de9175d2baa0b2
KimSuJIN <mrmagnet777@gmail.com>
2021-02-04 18:59:54 +0900

    create RecipeCollectionViewCell

---
+++ RecipeNote/RecipeNote/ViewControllers/RecipeCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCollectionViewCell.swift
+//  RecipeNote
+//
+//  Created by Kim on 2021/02/04.
+//  Copyright © 2021 Kim SuJin. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
