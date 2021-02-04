bd12f5a13f8a76b3ec87dcda7eb9a5a6749457cc
KimSuJIN <mrmagnet777@gmail.com>
2021-02-04 18:44:28 +0900

    connect first View with Main View for using Main View as first View

--- RecipeNote/RecipeNote/ViewController.swift
+++ RecipeNote/RecipeNote/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        toMain()
+    }
+    
+    func toMain() {
+        let vc = MainViewController(nibName: "MainViewController", bundle: nil)
+        vc.modalPresentationStyle = .fullScreen
+        self.present(vc, animated: false, completion: nil)
+    }
+    
 
 }
 
