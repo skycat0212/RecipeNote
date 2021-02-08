f16968028ce065fec17edd0c70e40b969aa896b7
KimSuJIN <mrmagnet777@gmail.com>
2021-02-08 16:17:06 +0900

    recipe collection view 및 cell 구현

--- RecipeNote/RecipeNote/ViewControllers/MainViewController.swift
+++ RecipeNote/RecipeNote/ViewControllers/MainViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var recipeCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
+        
+        recipeCollectionView.delegate = self
+        recipeCollectionView.dataSource = self
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        let nibName = UINib(nibName: "RecipeCollectionViewCell", bundle: nil)
+        recipeCollectionView.register(nibName, forCellWithReuseIdentifier: "recipeCell")
+        
+        
     }
 
 
@@ -26,5 +37,26 @@ class MainViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = recipeCollectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath) as! RecipeCollectionViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+//        return CGSize(width: self.view.bounds.width, height: self.view.bounds.height/3)
+//        return CGSize(width: 100, height: 150)
+//        return CGSize(width: (recipeCollectionView.superview?.bounds.height)!/3/3*2 , height: (recipeCollectionView.superview?.bounds.height)!/3)
+//        return CGSize(width: self.view.bounds.height/3/3*2, height: self.view.bounds.height/3)
+        return CGSize(width: recipeCollectionView.bounds.height/10/3*2*3 , height: recipeCollectionView.bounds.height/10*3)
+    }
 
 }
--- RecipeNote/RecipeNote/ViewControllers/RecipeCollectionViewCell.swift
+++ RecipeNote/RecipeNote/ViewControllers/RecipeCollectionViewCell.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class RecipeCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var foodImg: UIImageView!
+    @IBOutlet weak var foodName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setComponent() {
+//        foodImg =
+        foodName.text = "food Name"
+        
     }
 
 }
