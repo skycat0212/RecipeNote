a01c3fe246f7c4f8518b384aa7bb13ee69ef82da
KimSuJIN <mrmagnet777@gmail.com>
2021-03-09 08:32:33 +0900

    재료 추가하기 UI 수정

---
+++ RecipeNote/RecipeNote/cells/IngredientTagCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientTagCollectionViewCell.swift
+//  RecipeNote
+//
+//  Created by Kim on 2021/03/08.
+//  Copyright © 2021 Kim SuJin. All rights reserved.
+//
+
+import UIKit
+
+class IngredientTagCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
